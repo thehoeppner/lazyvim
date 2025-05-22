3da91d2f9e03d8542b19ef4918e3788be093d513
Lukas Hoeppner <lukas@hoeppner-dev.de>
2025-05-22 20:37:42 +0200

    changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,5 +10,28 @@ return {
     --keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
     --
     keys[#keys + 1] = { "K", false }
+
+    --Code Action
+    keys[#keys + 1] = {
+      "<leader>a",
+      vim.lsp.buf.code_action,
+      mode = "n",
+      desc = "LSP Code Action",
+    }
+
+    keys[#keys + 1] = {
+      "<leader>a",
+      vim.lsp.buf.code_action,
+      mode = "v",
+      desc = "LSP Code Action (Visual)",
+    }
+
+    --Hover
+    keys[#keys + 1] = {
+      "<leader>kk",
+      vim.lsp.buf.hover,
+      mode = "n",
+      desc = "LSP Hover",
+    }
   end,
 }
