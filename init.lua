0751b8b8e93a332eafbae4f7e356ee8f80175c6e
Lukas Hoeppner <lukas@hoeppner-dev.de>
2025-05-24 10:17:03 +0200

    init.lua set shellslash option on windows for debugging with netcoredbg

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local is_windows = vim.fn.has("win32") or vim.fn.has("win64") or vim.fn.has("win16")
+-- for some reason shellslash is being reset to true
+if is_windows then
+  vim.opt.shellslash = false
+  vim.defer_fn(function()
+    vim.opt.shellslash = false
+  end, 5000)
+end
