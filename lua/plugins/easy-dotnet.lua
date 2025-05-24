a5b2cee7cc6a1e830b99107d292dd21ff47aef69
Lukas Hoeppner <lukas@hoeppner-dev.de>
2025-05-24 18:52:01 +0200

    added basic dotnet keymaps

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -47,12 +47,11 @@ return {
           package = "",
         },
         mappings = {
-          run_test_from_buffer = { lhs = "<leader>dtr", desc = "run test from buffer" },
           filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
-          debug_test = { lhs = "<leader>dt", desc = "debug test" },
+          debug_test = { lhs = "d", desc = "debug test" },
           go_to_file = { lhs = "g", desc = "go to file" },
-          run_all = { lhs = "<leader>R", desc = "run all tests" },
-          run = { lhs = "<leader>r", desc = "run test" },
+          run_all = { lhs = "R", desc = "run all tests" },
+          run = { lhs = "r", desc = "run test" },
           peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
           expand = { lhs = "o", desc = "expand" },
           expand_node = { lhs = "E", desc = "expand node" },
@@ -60,7 +59,6 @@ return {
           collapse_all = { lhs = "W", desc = "collapse all" },
           close = { lhs = "q", desc = "close testrunner" },
           refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" },
-          debug_test_from_buffer = { lhs = "<leader>dd", desc = "debug all tests" },
         },
         --- Optional table of extra args e.g "--blame crash"
         additional_args = {},
@@ -118,13 +116,16 @@ return {
       dotnet.secrets()
     end, {})
 
-    -- Example keybinding
-    vim.keymap.set("n", "<C-p>", function()
-      dotnet.run_project()
-    end)
-
-    vim.keymap.set("n", "<leader>dtt", function()
+    vim.keymap.set("n", "<leader>Dt", function()
       dotnet.testrunner()
-    end)
+    end, { desc = "Open Dotnet Test Runner" })
+
+    vim.keymap.set("n", "<leader>Ds", function()
+      dotnet.build_solution()
+    end, { desc = "Build Solution" })
+
+    vim.keymap.set("n", "<leader>Dp", function()
+      dotnet.build()
+    end, { desc = "Build Project" })
   end,
 }
